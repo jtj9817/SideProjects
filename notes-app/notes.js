c0578664d26113d55c47c266821820a1b136d364
Josh J <joshuajadulco@gmail.com>
2018-10-13 21:49:06 -0400

    Updated addNotes() method

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,14 +1,21 @@
 console.log("Starting Notes.js app");
+const lodsh = require("lodash");
 //console.log(module);
 
 module.exports.age = 25;
-module.exports.addnote = () => {
-    console.log("Add note");
-    return 'New note';
+
+var addNote = (title, body) => {
+  console.log("Note Title: ", title);
+  console.log("Note: ", body);
 };
-module.exports.addNumber = (num1,num2) => {
-    var sum = num1 + num2;
-    console.log(`You wish to add the following values: ${num1} and ${num2}`);
-    console.log("Performing calculations");
-    return 'Result: ' + sum;
-}
+
+module.exports.addNumber = (num1, num2) => {
+  var sum = num1 + num2;
+  console.log(`You wish to add the following values: ${num1} and ${num2}`);
+  console.log("Performing calculations");
+  return "Result: " + sum;
+};
+
+module.exports = {
+  addNote
+};
